aa6f5542713640b9af353a806864588a95ee7ef6
Tsimafei Tarashkevich <tsimafei.tarashkevich@hansecom.com>
2022-05-17 15:51:07 +0300

    ref

--- app/src/main/java/com/fatbook/fatbookapp/ui/fragment/ViewRecipeFragment.java
+++ app/src/main/java/com/fatbook/fatbookapp/ui/fragment/ViewRecipeFragment.java
@@ -4,14 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.bumptech.glide.Glide;
+import com.fatbook.fatbookapp.R;
 import com.fatbook.fatbookapp.core.Recipe;
 import com.fatbook.fatbookapp.databinding.FragmentRecipeViewBinding;
 import com.fatbook.fatbookapp.ui.viewmodel.RecipeViewModel;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class ViewRecipeFragment extends Fragment {
 
@@ -21,28 +27,37 @@ public class ViewRecipeFragment extends Fragment {
 
     private RecipeViewModel recipeViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
-        binding = FragmentRecipeViewBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         recipeViewModel = new ViewModelProvider(getActivity()).get(RecipeViewModel.class);
 
         recipeViewModel.getSelectedRecipe().observe(getViewLifecycleOwner(), recipe -> {
             this.recipe = recipe;
         });
 
-//        recipe = feedViewModel.getSelectedRecipe().getValue();
+        recipe = recipeViewModel.getSelectedRecipe().getValue();
 
-//        Serializable recipe = getArguments().getSerializable("recipe");
-
-        return root;
+        binding.textViewFullRecipeName.setText(recipe.getName());
+        Glide
+                .with(getContext())
+                .load(recipe.getImage())
+                .into(binding.imageViewFullRecipeImage);
+        binding.textViewFullRecipeUsername.setText(recipe.getAuthor().getLogin());
+        String forks = Integer.toString(recipe.getForks());
+        binding.textViewFullRecipeForksQuantity.setText(forks);
+        binding.textViewFullRecipeDescription.setText(recipe.getDescription());
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentRecipeViewBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        MaterialToolbar toolbar = root.findViewById(R.id.toolbar_view_recipe);
+        toolbar.setNavigationOnClickListener(view -> {
+            NavHostFragment.findNavController(this).navigateUp();
+        });
+        return root;
     }
 
     @Override
