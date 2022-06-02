53492332eb0b58628ea3c28d38712d548840b159
mkvsk <tanysha6@me.com>
2022-06-02 21:11:34 +0200

    add: user edit mode image add/change/delete.

--- app/src/main/java/com/fatbook/fatbookapp/ui/fragment/UserProfileFragment.java
+++ app/src/main/java/com/fatbook/fatbookapp/ui/fragment/UserProfileFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.PorterDuff;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -17,6 +18,7 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -39,6 +41,8 @@ import com.google.android.material.snackbar.Snackbar;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.logging.Level;
 
 import lombok.extern.java.Log;
@@ -193,13 +197,21 @@ public class UserProfileFragment extends Fragment implements OnRecipeClickListen
         binding.editTextProfileName.setEnabled(allow);
         binding.editTextProfileBio.setEnabled(allow);
         if (allow) {
-            binding.buttonUserProfileAddPhoto.setVisibility(View.VISIBLE);
+            binding.imageViewUserProfilePhotoBgr.setVisibility(View.INVISIBLE);
+            binding.linearlayoutButtonsUserImage.setVisibility(View.VISIBLE);
             binding.editTextProfileName.setBackgroundResource(R.drawable.edit_mode_bgr);
             binding.editTextProfileBio.setBackgroundResource(R.drawable.edit_mode_bgr);
+
+            binding.buttonUserProfileChangePhoto.setVisibility(View.VISIBLE);
+            binding.buttonUserProfileDeletePhoto.setVisibility(View.VISIBLE);
         } else {
-            binding.buttonUserProfileAddPhoto.setVisibility(View.GONE);
+            binding.imageViewUserProfilePhotoBgr.setVisibility(View.VISIBLE);
+            binding.linearlayoutButtonsUserImage.setVisibility(View.GONE);
             binding.editTextProfileName.setBackgroundResource(R.drawable.round_corner_rect_white);
             binding.editTextProfileBio.setBackgroundResource(R.drawable.round_corner_rect_white);
+
+            binding.buttonUserProfileChangePhoto.setVisibility(View.GONE);
+            binding.buttonUserProfileDeletePhoto.setVisibility(View.GONE);
         }
     }
 
