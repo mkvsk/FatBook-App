a443e697b259133457ff97fc0d3a58dedc697010
mkvsk <tanysha6@me.com>
2022-07-28 18:08:02 +0200

    add: tab items

--- app/src/main/java/online/fatbook/fatbookapp/ui/fragment/TestFragment.kt
+++ app/src/main/java/online/fatbook/fatbookapp/ui/fragment/TestFragment.kt
@@ -1,14 +1,19 @@
 package online.fatbook.fatbookapp.ui.fragment
 
+import android.content.Context
+import android.graphics.drawable.StateListDrawable
+import android.location.GnssAntennaInfo
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.transition.AutoTransition
 import androidx.transition.Scene
 import androidx.transition.TransitionManager
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_test.*
 import online.fatbook.fatbookapp.R
 import online.fatbook.fatbookapp.databinding.FragmentTestBinding
@@ -18,6 +23,8 @@ class TestFragment : Fragment() {
     private var binding: FragmentTestBinding? = null
 
     private var expanded: Boolean = false
+
+    val tabCount: Int = tabLayout_userprofile.tabCount
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -52,5 +59,8 @@ class TestFragment : Fragment() {
                 expanded = false
             }
         }
+
+
     }
+
 }
