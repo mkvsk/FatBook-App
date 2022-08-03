c00861492ce72df8252ebb395f2e48d53d0c731c
mkvsk <tanysha6@me.com>
2022-08-03 13:16:21 +0200

    add: custom icon - password visibility

--- app/src/main/java/online/fatbook/fatbookapp/ui/fragment/EditProfileFragment.kt
+++ app/src/main/java/online/fatbook/fatbookapp/ui/fragment/EditProfileFragment.kt
@@ -2,11 +2,13 @@ package online.fatbook.fatbookapp.ui.fragment
 
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import kotlinx.android.synthetic.main.fragment_edit_profile.*
@@ -30,16 +32,16 @@ class EditProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-         /*val loadingDialog = LoadingDialog(requireContext())
+        /*val loadingDialog = LoadingDialog(requireContext())
 
-         loadingDialog.startLoading()
-         val handler = Handler()
-         handler.postDelayed(object : Runnable {
-             override fun run() {
-                 loadingDialog.isDismiss()
-             }
+        loadingDialog.startLoading()
+        val handler = Handler()
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                loadingDialog.isDismiss()
+            }
 
-         }, 5000)
+        }, 5000)
 */
 
         switch_app_theme.setOnCheckedChangeListener { darkTheme, isChecked ->
@@ -50,8 +52,49 @@ class EditProfileFragment : Fragment() {
             }
         }
 
+        nsv_edit_userprofile.setOnScrollChangeListener(object :
+            NestedScrollView.OnScrollChangeListener {
+            override fun onScrollChange(
+                v: NestedScrollView?,
+                scrollX: Int,
+                scrollY: Int,
+                oldScrollX: Int,
+                oldScrollY: Int
+            ) {
+                if (scrollY > oldScrollY) {
+                    Log.i("position:", "SCROLL DOWN")
+                }
+                if (scrollY < oldScrollY) {
+                    Log.i("position:", "SCROLL UP")
+                }
+                if (scrollY == 0) {
+                    Log.i("position:", "TOP SCROLL")
+                }
+                if (scrollY == v!!.measuredHeight - v!!.getChildAt(0).measuredHeight) {
+                    Log.i("position:", "BOTTOM SCROLL")
+                }
+            }
+
+        })
+
+
     }
 
 
+/*    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        nsv_edit_userprofile.setOnScrollChangeListener(object : NestedScrollView.OnScrollChangeListener {
+            override fun onScrollChange(
+                v: NestedScrollView?,
+                scrollX: Int,
+                scrollY: Int,
+                oldScrollX: Int,
+                oldScrollY: Int
+            ) {
+                println()
+            }
+
+        })
+    }*/
 
 }
