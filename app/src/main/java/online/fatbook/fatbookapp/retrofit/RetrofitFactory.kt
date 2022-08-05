5e30686e406003170c1ef8556b73f69273ee462d
Tsimafei Tarashkevich <tsimafei.tarashkevich@hansecom.com>
2022-08-05 15:45:39 +0300

    fix: remove api namespace

--- app/src/main/java/online/fatbook/fatbookapp/retrofit/RetrofitFactory.kt
+++ app/src/main/java/online/fatbook/fatbookapp/retrofit/RetrofitFactory.kt
@@ -5,20 +5,14 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitFactory {
-    private const val HTTP = "http://"
-    private const val ADDRESS = "10.0.2.2:1339/"
-
-    //    private static final String ADDRESS = "192.168.0.120:8080";
-    private const val ENDPOINT = "api/"
-    private const val URL_ENDPOINT = HTTP + ADDRESS + ENDPOINT
-    const val URL = HTTP + ADDRESS
+    private const val ADDRESS = "http://10.0.2.2:1339/"
     private val gson = GsonBuilder()
         .setLenient()
         .create()
 
     fun apiServiceClient(): NetworkInfoService {
         return Retrofit.Builder()
-            .baseUrl(URL_ENDPOINT)
+            .baseUrl(ADDRESS)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build().create(NetworkInfoService::class.java)
     }
