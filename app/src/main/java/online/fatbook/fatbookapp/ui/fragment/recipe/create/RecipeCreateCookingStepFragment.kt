161495535d24c1ebfdd6c5f6117c0dea57df1f3f
mkvsk <tanysha6@me.com>
2022-10-20 19:34:48 +0200

    fix: fill step description

--- app/src/main/java/online/fatbook/fatbookapp/ui/fragment/recipe/create/RecipeCreateCookingStepFragment.kt
+++ app/src/main/java/online/fatbook/fatbookapp/ui/fragment/recipe/create/RecipeCreateCookingStepFragment.kt
@@ -2,6 +2,7 @@ package online.fatbook.fatbookapp.ui.fragment.recipe.create
 
 import android.os.Bundle
 import android.text.Editable
+import android.text.InputFilter
 import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,11 +10,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.NavHostFragment
+import androidx.transition.AutoTransition
+import androidx.transition.Scene
+import androidx.transition.TransitionManager
 import kotlinx.android.synthetic.main.fragment_recipe_create_cooking_step.*
 import online.fatbook.fatbookapp.core.recipe.CookingStep
 import online.fatbook.fatbookapp.databinding.FragmentRecipeCreateCookingStepBinding
 import online.fatbook.fatbookapp.ui.adapters.CookingStepAdapter
 import online.fatbook.fatbookapp.ui.viewmodel.RecipeViewModel
+import online.fatbook.fatbookapp.util.hideKeyboard
 import online.fatbook.fatbookapp.util.obtainViewModel
 
 class RecipeCreateCookingStepFragment : Fragment() {
@@ -23,6 +28,8 @@ class RecipeCreateCookingStepFragment : Fragment() {
     private var adapter: CookingStepAdapter? = null
     private var cookingStep: CookingStep? = CookingStep()
     private var selectedCookingStep: CookingStep? = null
+
+    private var descriptionTextLength: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,6 +52,17 @@ class RecipeCreateCookingStepFragment : Fragment() {
             }
 
             override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                descriptionTextLength =
+                    edittext_recipe_create_cooking_step.filters.filterIsInstance<InputFilter.LengthFilter>()
+                        .firstOrNull()?.max!!
+                descriptionTextLength -= s.toString().length
+                textview_length_recipe_create_cooking_step.text = descriptionTextLength.toString()
+
+                TransitionManager.go(Scene(cardview_recipe_create_cooking_step), AutoTransition())
+
+                if (descriptionTextLength == 0) {
+                    hideKeyboard(edittext_recipe_create_cooking_step)
+                }
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -54,14 +72,17 @@ class RecipeCreateCookingStepFragment : Fragment() {
         })
 
         button_add_recipe_create_cooking_step.setOnClickListener {
+            val description = edittext_recipe_create_cooking_step.text.toString()
+                .replace("\\s+".toRegex(), " ")
             if (selectedCookingStep != null) {
                 selectedCookingStep!!.description =
-                    edittext_recipe_create_cooking_step.text.toString()
+                    description
+
                 adapter?.notifyItemChanged(recipeViewModel.selectedCookingStepPosition.value!!)
             } else {
                 var cookingStepsAmount = recipeViewModel.newRecipe.value!!.steps!!.size
 
-                cookingStep!!.description = edittext_recipe_create_cooking_step.text.toString()
+                cookingStep!!.description = description
                 cookingStep!!.stepNumber = ++cookingStepsAmount
 
                 recipeViewModel.newRecipe.value!!.steps!!.add(cookingStep!!)
