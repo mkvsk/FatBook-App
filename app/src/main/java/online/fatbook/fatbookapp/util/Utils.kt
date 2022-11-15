b0a4937c6f7d7e02d27acd67ea296869749c1b76
Tim Tarashkevich <t.tarashkevich@21vek.by>
2022-11-15 13:09:21 +0300

    add: editText clear text button

--- app/src/main/java/online/fatbook/fatbookapp/util/Utils.kt
+++ app/src/main/java/online/fatbook/fatbookapp/util/Utils.kt
@@ -1,7 +1,13 @@
 package online.fatbook.fatbookapp.util
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.MotionEvent
+import android.view.View
+import android.widget.EditText
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import online.fatbook.fatbookapp.R
 
 object Utils {
 
@@ -19,4 +25,25 @@ object Utils {
         return circularProgressDrawable
     }
 
+    fun EditText.setupClearButtonWithAction() {
+        addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(editable: Editable?) {
+                val clearIcon = if (editable?.isNotEmpty() == true) R.drawable.ic_clear_text else 0
+                setCompoundDrawablesWithIntrinsicBounds(0, 0, clearIcon, 0)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+        })
+
+        setOnTouchListener(View.OnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (event.rawX >= (this.right - this.compoundPaddingRight)) {
+                    this.setText("")
+                    return@OnTouchListener true
+                }
+            }
+            return@OnTouchListener false
+        })
+    }
 }
