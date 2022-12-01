3c4cb1e5ddcef97c3655a125075e4ea176e2ec87
mkvsk <tanysha6@me.com>
2022-12-01 16:05:06 +0200

    fix: user profile UI. add: link regex

--- app/src/main/java/online/fatbook/fatbookapp/util/Constants.kt
+++ app/src/main/java/online/fatbook/fatbookapp/util/Constants.kt
@@ -7,6 +7,8 @@ object Constants {
     const val SYMBOL_AT = '@'
     const val PASSWORD_REGEX = "^(?=\\S+\$).{6,20}$"
     const val USERNAME_REGEX = "^(?=.{3,20}\$)(?![_.])(?!.*[_.]{2})[a-z0-9._]+(?<![_.])\$"
+    const val USER_PRETTY_WEBSITE_REGEX = "^(http[s]?://www\\.|http[s]?://|www\\.)"
+
     const val REFRESH_TOKEN = "refresh_token"
     const val SP_TAG = "shared_preferences"
     const val SP_TAG_PASSWORD = "password"
@@ -22,5 +24,11 @@ object Constants {
     const val TAG_SELECT_ALL_BUTTON = "select_all_button"
 //    const val ET_BIO_REGEX = "^(?=\\S+\$).{0,635}$"
 
-    val rootDestinations = setOf(R.id.feed_dest, R.id.search_dest, R.id.recipe_first_stage_dest, R.id.notification_dest, R.id.user_profile_dest)
+    val rootDestinations = setOf(
+        R.id.feed_dest,
+        R.id.search_dest,
+        R.id.recipe_first_stage_dest,
+        R.id.notification_dest,
+        R.id.user_profile_dest
+    )
 }
